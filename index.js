c585598da3481204216a67de514a47a49b2ff89e
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-10 12:44:01 -0400

    iterating over obj props

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 let loaf = {
-	flour: 300,
-	water: 210
-}
+  flour: 300,
+  water: 210
+};
 
 console.log(loaf.flour);
 console.log(loaf.water);
 
-loaf.hydration = function(){
-	return (this.water / this.flour) * 100;
+loaf.hydration = function() {
+  return (this.water / this.flour) * 100;
 };
 
 console.log(loaf.hydration());
+
+const obj = { foo: 1, bar: 2, fum: 3, quux: 4, spam: 5 };
+
+const consoler = object => {
+  for (let key in object) {
+    console.log(`${key}: ${object[key]}`);
+  }
+};
+
+consoler(obj);
