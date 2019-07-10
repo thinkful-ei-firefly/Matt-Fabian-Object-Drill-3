4632f0cfd0fe9f7d0c4595498e2d43e0585c954c
Fabian Lema <fabianlemac@gmail.com>
2019-07-10 11:38:32 -0500

    Hydration

---
+++ index.js
@@ -0,0 +1,13 @@
+let loaf = {
+	flour: 300,
+	water: 210
+}
+
+console.log(loaf.flour);
+console.log(loaf.water);
+
+loaf.hydration = function(){
+	return (this.water / this.flour) * 100;
+};
+
+console.log(loaf.hydration());
